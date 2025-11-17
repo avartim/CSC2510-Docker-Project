63a6c78443dd4905a33ea83d643b6b7f27844fc3
avartim <tdchow42@tntech.edu>
2025-11-16 23:20:33 -0600

    Completed Project 2

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 		log.Fatal("Cannot connect to database:", err)
 	}
 	log.Println("Connected to PostgreSQL!")
-
+	log.Println("Server started by Timothy Chow")
 	http.HandleFunc("/users", getUsers)
 	http.HandleFunc("/add", addUser)
 
